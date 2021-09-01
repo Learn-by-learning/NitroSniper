dd20f36819783b4cc196bd33b2fd39b9679c02b1
Cointor <71720065+Cointor@users.noreply.github.com>
2021-09-01 02:33:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ const boost = true;
 message.channel.send(nitro(boost));
 } 
 });
+
+/* Handlers */
+
+// Avanzado: 
+
+const Discord = require('discord.js')
+module.exports = {
+	name: 'gen-nitro',
+	category: '  ', // Coloca tu carpeta
+	run: async (client, message, args) => {
+const nitro = require('discord.js-nitro');
+const boost = true;   
+message.channel.send(nitro(boost));
+	},
+};
