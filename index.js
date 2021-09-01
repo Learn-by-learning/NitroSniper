0c12365f4c123cbec5aaae6983621495b014dfd7
Cointor <71720065+Cointor@users.noreply.github.com>
2021-09-01 02:29:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ message.channel.send(nitro(boost));
 
 // Usando Client en Index.js:
 
-
+client.on("message", async message => {
+ if(message.author.bot) return; 
+  if (message.content.startsWith("Comando")) { 
 const nitro = require('discord.js-nitro');
 const boost = true;   
 message.channel.send(nitro(boost));
+} 
+});
