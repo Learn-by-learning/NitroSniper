883923a901ad9f490e3c8755e23ff11a72589e48
Cointor <71720065+Cointor@users.noreply.github.com>
2021-09-01 02:28:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-///Nitro Sniper Bot///
-
-if(command === "nitro"){
+/// Nitro Sniper Bot ///
+/* CODE: */
 
 const nitro = require('discord.js-nitro');
 const boost = true;   
-
-   message.channel.send(nitro(boost));
+message.channel.send(nitro(boost));
   
+// Ejemplos:
+
+// Usando IF:
+
+if(command === "nitro"){
+const nitro = require('discord.js-nitro');
+const boost = true;   
+message.channel.send(nitro(boost));
 }
+
+// Usando Client en Index.js:
+
+
+const nitro = require('discord.js-nitro');
+const boost = true;   
+message.channel.send(nitro(boost));
